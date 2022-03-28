80809c952e319c4d771ba3b42fe7087317477421
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-28 13:45:27 +0600

    set card title

--- src/component/Cart/Card2.js
+++ src/component/Cart/Card2.js
@@ -6,7 +6,7 @@ const Card2 = ({ product }) => {
         <Card>
             <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title>Card title: {product.name} </Card.Title>
                 <Card.Text>
                     This is a wider card with supporting text below as a natural lead-in to
                     additional content. This content is a little bit longer.
